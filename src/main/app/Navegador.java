2275b32b1b3757e3c14c56f8a124981f738d2c03
caiquepirs <c.pires@ba.estudante.senai.br>
2025-03-23 05:56:39 -0300

    Criando interface Navegador para representar o navegador web do iphone

---
+++ src/main/app/Navegador.java
@@ -0,0 +1,7 @@
+public interface Navegador {
+
+    public void adicionarNovaAba();
+    public void exibirPagina(String url);
+    public void pesquisar(String texto);
+    public void atualizarPagina();
+}
