55095089fe83018a200b429757e9ee2ad84036bb
caiquepirs <c.pires@ba.estudante.senai.br>
2025-03-23 05:50:29 -0300

    Criando a interface que representa o aplicativo de telefone (chamadas) do iphone

---
+++ src/main/app/Telefone.java
@@ -0,0 +1,8 @@
+public interface Telefone {
+
+    public void atender();
+    public void ligar(String numero);
+    public void desligar();
+    public String adicionarTelefone(String telefone);
+    public void excluirTelefone(String telefone);
+}
